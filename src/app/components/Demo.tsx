3ea274c00eecf25f6893030ee292341ee098c039
jawwad-ali <alijawwad001@gmail.com>
2023-04-28 20:43:00 +0500

    article summary implemented

--- src/app/components/Demo.tsx
+++ src/app/components/Demo.tsx
@@ -1,12 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
 
-import { FaCopy, FaLink } from "react-icons/fa";
-import {
-  AiOutlineLoading3Quarters,
-  AiOutlineCheck,
-  AiOutlineSend,
-} from "react-icons/ai";
+import { FaCopy, FaLink, FaSpinner } from "react-icons/fa";
+import { AiOutlineSend } from "react-icons/ai";
 
 import { useLazyGetSummaryQuery } from "@/services/article";
 
@@ -21,18 +17,45 @@ const obj: IArticle = {
 };
 
 const Demo = () => {
-  const [article, setArticle] = useState(obj);
+  // single articles
+  const [article, setArticle] = useState<IArticle>(obj);
+
+  // store all articles
+  const [allArticles, setAllArticles] = useState<IArticle[]>([]);
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    // getting articles to local storage
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles") as string
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  // submitting form
   const handleSubmit = async (e: any) => {
-    e.preventDefault()
-    
+    e.preventDefault();
+
     const { data } = await getSummary({ articleUrl: article.url });
 
     if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
+      const newArticle = {
+        ...article,
+        summary: data.summary,
+      };
+
+      const updateAllArticles = [newArticle, ...allArticles];
+
       setArticle(newArticle);
+      setAllArticles(updateAllArticles);
+
+      // setting the article to localstorage
+      localStorage.setItem("articles", JSON.stringify(updateAllArticles));
+
       console.log(newArticle);
     }
   };
@@ -60,6 +83,54 @@ const Demo = () => {
             <AiOutlineSend />
           </button>
         </form>
+
+        {/* Browse URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item: IArticle, idx: number) => (
+            <div
+              key={`link-${idx}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <FaCopy className="w-[40%] h-[40%] object-contain text-gray-700" />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 text-sm font-medium truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Display Results */}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {/* {article.summary} */}
+        {isFetching ? (
+          <>
+            <FaSpinner className="w-20 h-20 object-contain" />
+            <h4 className="font-extrabold text-3xl text-center">Fetching...</h4>
+          </>
+        ) : error ? (
+          <p>
+            Well, that wasn't supposed to be happen...
+            <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
--- src/services/article.ts
+++ src/services/article.ts
@@ -2,19 +2,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const articleApi: any = createApi({
     reducerPath: "articleApi",
-
-    baseQuery: fetchBaseQuery({ 
+ 
+    baseQuery: fetchBaseQuery({  
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers:any) => { 
             headers.set('X-RapidAPI-Key', process.env.RAPID_API_KEY),
                 headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com')
-            return headers
+            return headers  
         }
     }),
 
     endpoints: (builder) => ({
         getSummary: builder.query<any, string>({
-            query: (params:any) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=1`,
+            query: (params:any) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
         }),
     }),
 }) 
